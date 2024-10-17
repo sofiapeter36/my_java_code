8b70d7374a558cbb2eb7cf56b007694cdeb52e56
sofia <sofia@SofiaJames.domain.name>
2024-10-17 16:00:17 +0530

    1st commit

---
+++ Java_programs/src/Collections/HashMap.java
@@ -0,0 +1,23 @@
+package Collections;
+
+import java.security.KeyStore.Entry;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class HashMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String,String> mp=new LinkedHashMap<String,String>();
+		mp.put("sofia", "James");
+		mp.put("sandhya", "Venkatesh");
+		mp.put("Jainy", "Peter");
+		
+		Iterator <Map.Entry<String,String>> it= mp.entrySet().iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+
+}
